2e28ae769890b7c24cf25a464b9a66db57d47854
lain-making-bread <hhichri60@gmail.com>
2023-11-01 17:28:57 +0100

    added model choices

--- app.py
+++ app.py
@@ -9,13 +9,40 @@ import gradio as gr
 from discord.ext import commands
 import gradio_client as grc
 from gradio_client.utils import QueueError
-# from PIL import Image
+from typing import Literal
 
 dotenv.load_dotenv()
 event = Event()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+# Limited to only 25 models
+models = Literal['3Guofeng3_v34.safetensors [50f420de]', 
+        'absolutereality_V16.safetensors [37db0fc3]', 
+        'absolutereality_v181.safetensors [3d9d4d2b]',
+        'anythingV5_PrtRE.safetensors [893e49b9]', 
+        'blazing_drive_v10g.safetensors [ca1c1eab]', 
+        'cetusMix_Version35.safetensors [de2f2560]',
+        'childrensStories_v1ToonAnime.safetensors [2ec7b88b]', 
+        'Counterfeit_v30.safetensors [9e2a8f19]', 
+        'cuteyukimixAdorable_midchapter3.safetensors [04bdffe6]', 
+        'cyberrealistic_v33.safetensors [82b0d085]',
+        'dreamlike-anime-1.0.safetensors [4520e090]', 
+        'dreamlike-photoreal-2.0.safetensors [fdcf65e7]',
+        'dreamshaper_8.safetensors [9d40847d]', 
+        'edgeOfRealism_eorV20.safetensors [3ed5de15]', 
+        'elldreths-vivid-mix.safetensors [342d9d26]', 
+        'epicrealism_naturalSinRC1VAE.safetensors [90a4c676]', 
+        'juggernaut_aftermath.safetensors [5e20c455]', 
+        'lofi_v4.safetensors [ccc204d6]', 
+        'lyriel_v16.safetensors [68fceea2]', 
+        'neverendingDream_v122.safetensors [f964ceeb]', 
+        'openjourney_V4.ckpt [ca2f377f]', 
+        'pastelMixStylizedAnime_pruned_fp16.safetensors [793a26e8]',
+        'Realistic_Vision_V5.0.safetensors [614d1063]', 
+        'revAnimated_v122.safetensors [3f4fefd9]', 
+        'rundiffusionFX25D_v10.safetensors [cd12b0ee]',
+        ]
 async def wait(job):
     while not job.done():
         await asyncio.sleep(0.2)
@@ -40,7 +67,7 @@ async def on_ready():
     print("------")
 
 
-async def text2img(pos_prompt: str, neg_promt: str = ""):
+async def text2img(pos_prompt: str, neg_promt: str = "",model="absolutereality_v181.safetensors [3d9d4d2b]"):
     """
     Generates an image based on the given positive prompt and optional negative prompt.
 
@@ -55,7 +82,7 @@ async def text2img(pos_prompt: str, neg_promt: str = ""):
     txt2img_conf = {
         "parameter_11" : pos_prompt,
         "parameter_12" : neg_promt,
-        "stable_diffusion_checkpoint" : "3Guofeng3_v34.safetensors [50f420de]",
+        "stable_diffusion_checkpoint" : model,
         "sampling_steps" : 25,
         "sampling_method" : "DPM++ 2M Karras",
         "cfg_scale" : 7,
@@ -75,7 +102,7 @@ async def text2img(pos_prompt: str, neg_promt: str = ""):
     return job.result()
     
     
-async def img2img(pos_prompt: str, neg_promt: str = "", img = None):
+async def img2img(pos_prompt: str, neg_promt: str = "", img = None,model="absolutereality_v181.safetensors [3d9d4d2b]"):
     """
     Generates an image based on the given positive prompt, optional negative prompt and image path.
 
@@ -93,7 +120,7 @@ async def img2img(pos_prompt: str, neg_promt: str = "", img = None):
         "denoising_strength" : 0.7,
         "parameter_44" : pos_prompt,
         "parameter_45" : neg_promt,
-        "stable_diffusion_checkpoint" : "3Guofeng3_v34.safetensors [50f420de]",
+        "stable_diffusion_checkpoint" : model,
         "sampling_steps" : 25,
         "sampling_method" : "DPM++ 2M Karras",
         "cfg_scale" : 7,
@@ -112,15 +139,15 @@ async def img2img(pos_prompt: str, neg_promt: str = "", img = None):
     await wait(job)
     return job.result()
 
-def run_dffusion(pos_prompt: str,neg_promt: str = "",img_url= None):
+def run_dffusion(pos_prompt: str,neg_promt: str = "",img_url= None,model="absolutereality_v181.safetensors [3d9d4d2b]"):
     """Runs the diffusion model."""
 
     if img_url == None:
         # Support for text prompts
-        generated_image = text2img(pos_prompt, neg_promt)
+        generated_image = text2img(pos_prompt, neg_promt,model)
     else:
         # Support for image prompts
-        generated_image = img2img(pos_prompt, neg_promt, img_url)
+        generated_image = img2img(pos_prompt, neg_promt, img_url,model)
 
     return generated_image
 
@@ -129,15 +156,38 @@ def run_dffusion(pos_prompt: str,neg_promt: str = "",img_url= None):
     name="diffusion",
     description="creates an AI generated image"
 )
-async def diffusion(ctx, pos_prompt: str="",neg_promt: str = "", img_url= None):
+async def diffusion(ctx, pos_prompt: str="",neg_promt: str = "", img_url= None,improve : Literal["True","False"] = "True",model : models = "absolutereality_v181.safetensors [3d9d4d2b]"):
     """Creates an AI generated image based on a prompt."""
+    # string injection for improved results
+    _improved_pos_prompt = "ultrarealistic,8k"
+    _improved_neg_promt = "3d, cartoon, (deformed eyes, nose, ears, nose), bad anatomy, ugly,blur"
     try :
-        await ctx.reply(f"generating image for { ctx.author.mention } ... \npos_prompt: {pos_prompt} \nneg_prompt: {neg_promt} \nimage_url: `{img_url}`")
-        result = await run_dffusion(pos_prompt , neg_promt,img_url)
+        pos_prompt =pos_prompt.strip()
+        neg_promt = neg_promt.strip()
+        # check if the user wants to improve the results
+        if improve == "True":
+            if len(pos_prompt) == 0:
+                pos_prompt = _improved_pos_prompt
+            elif pos_prompt[-1] != ",": 
+                pos_prompt += "," + _improved_pos_prompt
+            else: 
+                pos_prompt += _improved_pos_prompt
+            if len(neg_promt) == 0:
+                neg_promt = _improved_neg_promt
+            elif neg_promt[-1] != ",":
+                neg_promt += "," + _improved_neg_promt
+            else:
+                neg_promt += _improved_neg_promt
+        # show the configuration
+        await ctx.reply(f"""
+                        generating image for { ctx.author.mention } ... \
+                            ```pos_prompt: {pos_prompt} \nneg_prompt: {neg_promt}\ncontrol image_url: {img_url}\nmodel: {model}```
+                            """)
+        # run the API
+        result = await run_dffusion(pos_prompt , neg_promt,img_url,model=model)
         await ctx.channel.send(file=discord.File(result))
     except Exception as e:
-        await ctx.send("Error occured while running the model. Please try again later.")
-        await ctx.send(e)
+        await ctx.send(f"Error occured while running the model. Please try again later.\n{e}")
         return
 
 
